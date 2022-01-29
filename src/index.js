2a70c89b10309f51db3d1814009b0dca9701233e
Alexei Marusov <88613624+MarusovAlexei@users.noreply.github.com>
2022-01-29 13:39:28 +0300

    feat: add result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arrayValue = bracketsConfig.map((element) => element.join(''))
+  let flag;
+
+  for (let i = 0; i < arrayValue.length;) {
+    if (str.includes(arrayValue[i])) {
+      str = str.replace(arrayValue[i], '')
+      i = 0
+    }
+    else { i++ }
+  }
+
+  flag = str.length > 0 ? false : true;
+
+  return flag;
 }
